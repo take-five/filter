91faf2b3de84631ba07ce4c11eed6ee6d741dd58
amikhailov <amikhailov83@gmail.com>
2011-12-12 02:26:44 +0600

    Removed deferred_enum dependency, removed lazyness

--- lib/filter.rb
+++ lib/filter.rb
@@ -1,5 +1,3 @@
-require 'deferred_enum'
-
 # == Synpsys
 # Extension for Ruby Enumerable module
 #
@@ -7,12 +5,12 @@ require 'deferred_enum'
 # Extends Enumerable module with +filter+ method - enhanced version of Enumerable#select
 #
 # == Examples
-#   [1, 2, 3, 'ab'].filter(/a/)               # => #<Enumerator: ['ab']>
-#   [1, 2, 3].filter(&:even?)                 # => #<Enumerator: [2]>
-#   [1, 2, 3, 4.2].filter :to_i => :even?     # => #<Enumerator: [2, 4]>
-#   [1, 2, 4].filter { |num| num.even? }      # => #<Enumerator: [2, 4]>
-#   [1, 2, 4].filter(:even?) { |n| n + 1 }    # => #<Enumerator: [3, 5]>
-#   [0, false, 2, nil].filter(true)           # => #<Enumerator: [0, 2]>
+#   [1, 2, 3, 'ab'].filter(/a/)               # => ['ab']
+#   [1, 2, 3].filter(&:even?)                 # => [2]
+#   [1, 2, 3, 4.2].filter :to_i => :even?     # => [2, 4]
+#   [1, 2, 4].filter { |num| num.even? }      # => [2, 4]
+#   [1, 2, 4].filter(:even?) { |n| n + 1 }    # => [3, 5]
+#   [0, false, 2, nil].filter(true)           # => [0, 2]
 module Enumerable
   # Extended Enumerable#select combibed with Enumerable#collect
   #
@@ -25,24 +23,24 @@ module Enumerable
     pattern, block = block, nil if block_given? && pattern.nil?
 
     filtered = case pattern
-      when NilClass then defer
+      when NilClass then self
 
       when Class, Module then
-        defer.select do |e|
+        select do |e|
           e.is_a?(Class) || e.is_a?(Module) ?
               e <= pattern :
               e.is_a?(pattern)
         end
 
-      when Symbol, Proc, Method then defer.select(&pattern.to_proc)
+      when Symbol, Proc, Method then select(&pattern.to_proc)
 
       when Array then # enum.filter [:even?, :positive?, :cool?, proc { |n| n < 10 }]
-        pattern.reduce(self.defer) do |chain, local_pattern|
+        pattern.reduce(self) do |chain, local_pattern|
           chain.select(&local_pattern.to_proc)
         end
 
       when Hash then # enum.filter :to_i => :even?, :ceil => :odd?
-        pattern.reduce(self.defer) do |chain, pair|
+        pattern.reduce(self) do |chain, pair|
           attr, local_pattern = pair.map(&:to_proc)
 
           chain.select do |element|
@@ -50,9 +48,9 @@ module Enumerable
           end
         end
 
-      when TrueClass, FalseClass then defer.select { |e| !!e == pattern }
+      when TrueClass, FalseClass then select { |e| !!e == pattern }
 
-      else defer.select { |e| pattern === e } # otherwise - String, Regexp, Numeric etc.
+      else select { |e| pattern === e } # otherwise - String, Regexp, Numeric etc.
     end
 
     # also transform elements if block given
--- spec/filter_spec.rb
+++ spec/filter_spec.rb
@@ -1,39 +1,39 @@
 require File.expand_path('../test_helper', __FILE__)
 
 describe Enumerable do
-  it "should return Enumerator" do
+  it "should return Enumerable" do
     result = [1, 2, 3].filter
 
-    result.should be_a(Enumerator)
+    result.should be_a(Enumerable)
   end
 
   it "should return equivalent Enumerator on null input" do
-    [1, 2].filter.to_a.should == [1, 2]
+    [1, 2].filter.should == [1, 2]
   end
 
   it "should match regular expressions" do
-    [1, 2, 3, 'ba'].filter(/ba/).to_a.should == ['ba']
+    [1, 2, 3, 'ba'].filter(/ba/).should == ['ba']
   end
 
   it "should match against symbols" do
-    [1, 2, 3].filter(:even?).to_a.should == [2]
+    [1, 2, 3].filter(:even?).should == [2]
   end
 
   it "should match against lambdas" do
-    [1, 2, 3].filter(lambda{|n| n.even?}).to_a.should == [2]
+    [1, 2, 3].filter(lambda{|n| n.even?}).should == [2]
   end
 
   it "should match against blocks" do
-    [1, 2, 3].filter { |n| n.even? }.to_a.should == [2]
+    [1, 2, 3].filter { |n| n.even? }.should == [2]
   end
 
   it "should support #map" do
-    [1, 2, 3].filter(:even?) { |n| n + 1 }.to_a.should == [3]
+    [1, 2, 3].filter(:even?) { |n| n + 1 }.should == [3]
   end
 
   it "should match against Hash" do
-    [1, 2, 3, 4.2].filter(:to_i => :even?).to_a.should == [2, 4.2]
-    [2, 3, 4.2].filter(:to_i => :even?, :ceil => :odd?).to_a.should == [4.2]
+    [1, 2, 3, 4.2].filter(:to_i => :even?).should == [2, 4.2]
+    [2, 3, 4.2].filter(:to_i => :even?, :ceil => :odd?).should == [4.2]
   end
 
   it "should match against Class and Module" do
@@ -41,16 +41,16 @@ describe Enumerable do
     m = Module.new {}
     i = c.new
 
-    [c, i, 1].filter(c).to_a.should == [c, i]
-    [c, m, i, 1].filter(m).to_a.should == [m]
+    [c, i, 1].filter(c).should == [c, i]
+    [c, m, i, 1].filter(m).should == [m]
   end
 
   it "should match against Array" do
-    [0, 1].filter([:even?, :zero?]).to_a.should == [0]
+    [0, 1].filter([:even?, :zero?]).should == [0]
   end
 
   it "should match against true or false" do
-    [0, false, 2, nil].filter(true).to_a.should == [0, 2]
-    [0, false, 2, nil].filter(false).to_a.should == [false, nil]
+    [0, false, 2, nil].filter(true).should == [0, 2]
+    [0, false, 2, nil].filter(false).should == [false, nil]
   end
 end
